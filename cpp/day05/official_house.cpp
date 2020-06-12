e0c34048eefb946862bcf5482da3bf734ea43f7b
AstPy-ms <sketdance551@gmail.com>
2020-06-12 17:42:09 +0900

    open day05

---
+++ cpp/day05/official_house.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+
+using namespace std;
+
+// b棟 f階 の r番目 の部屋に v人 が追加で入居したことを示します
+
+int main(){
+
+    // i棟目のj階k号室
+    unsigned int n;
+    unsigned int i, j, k;
+    unsigned int b, f, r;
+    int v;
+    unsigned int houses[4][3][10] = {0};
+
+    cin >> n;
+
+    for( i = 0; i < n; i++ ){
+        cin >> b >> f >> r >> v;
+        houses[b-1][f-1][r-1] += v;
+    }
+
+    // 表示
+    for( i = 0; i < 4; i++ ){
+        for( j = 0; j < 3; j++ ){
+            for( k = 0; k < 10; k++ ){
+                cout << " " <<houses[i][j][k];
+            }
+            cout << endl;
+        }
+        if( i != 3 ){
+            for( j = 0; j < 20; j++){
+                cout << "#";
+            }
+            cout << endl;
+        }
+    }
+
+    return 0;
+
+}
